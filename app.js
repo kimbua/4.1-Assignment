6143344c45fff230e51b8f1d09ee9fac235914a4
Nguyen Lien <nguyen.k.lien98@gmail.com>
2021-07-14 09:25:40 +0700

    nodemon

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
+
+
 const logger = require("morgan");
 app.use(logger("dev"));
 const app = express();
 const port = 5000;
 
-let { jobs } = require("./data.json")
+const foos = require("./foos.json")
+
+const { jobs } = require("./data.json")
+
 
 app.get("/", (req, res) => {
   res.send("Hello Programmers");
